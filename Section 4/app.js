fa6036ae3c89b1798a448e428c39e01d694440eb
Ainun Abidin <ainun.ilabaca@gmail.com>
2019-06-20 10:28:03 +0700

    section 4

---
+++ Section 4/app.js
@@ -0,0 +1,42 @@
+//contoh tutorila no9
+//console.log('Hello world..');
+//const fs = require('fs');
+//fs adalah library filesystem di node js
+//fs.writeFileSync('notes1.txt','ini ada file cobaan untuk belajar Node.js');
+// fs.writeFileSync untuk emalkukan penulisan synchronous
+//fs.appendFileSync('notes1.txt','katanya mereka cuma tau seidkit');
+//melakukan penambahan data yang telah ada
+////////////////////////////////////////////////////////////////////////////////////
+
+//contoh tutorial no 10 
+//
+//const add = require('./no10.js')
+//1.melakukan pemanggilan data lain ke dalam variable
+//const sum = add(8, 2)
+//2.melakukan pemanggilan fungsi dari file no1.js yang lain
+//console.log(sum)
+//menampilkan variable
+//chalenge//
+// const getNotes = require('./note.js')
+// console.log(coba = getNotes())
+//////////////////////////////////////////////////////////////////////////////////
+
+//contoh tutorial no 11 section 3
+//
+//const validator = require('validator')
+//console.log(validator.isEmail('ainun.ilabaca@gmail.com'))
+//console.log(validator.isURL('www.google.com'))
+//
+//chalenge
+
+///////////////////////////////////////////////////////////////////////////
+
+//contoh tutorila no 12 section 3
+// const chalk = require('chalk')
+// console.log(chalk.bold.white('success!!!'))
+/////////////////////////////////////////////////////////////////////////
+
+//contoh tutorial no 13 section 3
+const chalk = require('chalk')
+console.log(chalk.green.inverse.bold('Success'))
+
