a9e65b8718d6036b1245e76751c150671a8b0e0c
Ainun Abidin <ainun.ilabaca@gmail.com>
2019-06-20 11:23:32 +0700

    "section 4 no 16"

--- Section 4/app.js
+++ Section 4/app.js
@@ -1,42 +1,12 @@
-//contoh tutorila no9
-//console.log('Hello world..');
-//const fs = require('fs');
-//fs adalah library filesystem di node js
-//fs.writeFileSync('notes1.txt','ini ada file cobaan untuk belajar Node.js');
-// fs.writeFileSync untuk emalkukan penulisan synchronous
-//fs.appendFileSync('notes1.txt','katanya mereka cuma tau seidkit');
-//melakukan penambahan data yang telah ada
-////////////////////////////////////////////////////////////////////////////////////
-
-//contoh tutorial no 10 
-//
-//const add = require('./no10.js')
-//1.melakukan pemanggilan data lain ke dalam variable
-//const sum = add(8, 2)
-//2.melakukan pemanggilan fungsi dari file no1.js yang lain
-//console.log(sum)
-//menampilkan variable
-//chalenge//
-// const getNotes = require('./note.js')
-// console.log(coba = getNotes())
-//////////////////////////////////////////////////////////////////////////////////
-
-//contoh tutorial no 11 section 3
-//
-//const validator = require('validator')
-//console.log(validator.isEmail('ainun.ilabaca@gmail.com'))
-//console.log(validator.isURL('www.google.com'))
-//
-//chalenge
-
-///////////////////////////////////////////////////////////////////////////
-
-//contoh tutorila no 12 section 3
-// const chalk = require('chalk')
-// console.log(chalk.bold.white('success!!!'))
-/////////////////////////////////////////////////////////////////////////
-
-//contoh tutorial no 13 section 3
 const chalk = require('chalk')
-console.log(chalk.green.inverse.bold('Success'))
+const yarg = require('yarg')
+console.log(chalk.blue.inverse.bold('water'))
 
+// section 3 no 15
+// const command = process.argv[2]
+// console.log(process.argv)
+// if (command === 'add'){
+//     console.log('Halooo people')
+// }else if (command === 'remove'){
+//     console.log('kunaoon jangg....')
+// }
