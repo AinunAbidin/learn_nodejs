1de19589e6656ee64c3bdce0b047cc11f75986eb
Ainun Abidin <ainun.ilabaca@gmail.com>
2019-06-21 10:05:10 +0700

    section 4 no 18

---
+++ Section 4/Json/01_json.js
@@ -0,0 +1,40 @@
+// const fs = require('fs')
+// const film = {
+//     title : 'harry poter',
+//     author : 'advonchiki'
+// }
+
+// const data_film = JSON.stringify(film)
+// console.log(data_film)
+
+// const parse = JSON.parse(data_film)
+// console.log(parse.author)
+
+// fs.writeFileSync('01_Json.json' , data_film)
+// const data = fs.readFileSync('01_Json.json')
+// const dataJson = data.toString()
+// const data1 = JSON.parse(dataJson)
+// console.log(data1.title)
+///////////////////////////////
+
+// soal section 4 no 18
+// challenge
+// const fs = require('fs')
+// const bio = {
+//     nama : 'ainun abidin',
+//     age : '20',
+//     school : 'Telkom University'
+// }
+// const biodata = JSON.stringify(bio)
+// console.log(biodata)
+// fs.writeFileSync('coba.json',biodata)
+// const databaca = fs.readFileSync('./coba.json')
+// const datao = JSON.parse(databaca)
+// console.log(datao)
+// datao.nama = 'Haikal ganteng'
+// datao.age = 20
+// console.log(datao)
+// const biodata1 = JSON.stringify(datao)
+// console.log(biodata1)
+// fs.writeFileSync('coba.json',biodata1)
+
